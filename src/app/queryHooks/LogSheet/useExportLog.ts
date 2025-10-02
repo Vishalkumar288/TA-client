6cb01c0ccfa311cf35d310dba7b2b427cffd4bb1
vishal-kumar-1 <141398064+vishal-kumar-1@users.noreply.github.com>
2025-10-03 01:14:08 +0530

    bug fix

--- src/app/queryHooks/LogSheet/useExportLog.ts
+++ src/app/queryHooks/LogSheet/useExportLog.ts
@@ -4,7 +4,19 @@ import { AxiosError } from "axios";
 
 export const useExportLog = () => {
   const mutation = useMutation<any, AxiosError<{ message: string }>, any>({
-    mutationFn: ({ data }: any) => excelLog(data)
+    mutationFn: ({ data }: any) => excelLog(data),
+    onError: (error) => {
+      // If backend sent JSON but axios returned ArrayBuffer
+      if (error.response?.data instanceof ArrayBuffer) {
+        try {
+          const decoded = new TextDecoder().decode(error.response.data);
+          const parsed = JSON.parse(decoded);
+          error.response.data = parsed; // overwrite with parsed JSON
+        } catch (e) {
+          console.error("Failed to parse error buffer:", e);
+        }
+      }
+    }
   });
 
   return mutation;
